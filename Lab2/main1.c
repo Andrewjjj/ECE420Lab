657b1e16d7e7f954af2c490fe83f9bfea4e82cfa
Tyler Beckingham <tyler@Tylers-MacBook-Pro.local>
2021-02-13 16:32:22 -0700

    test

--- Lab2/main1.c
+++ Lab2/main1.c
@@ -9,8 +9,11 @@
 #include <string.h>
 #include "timer.h"
 #include "common.h"
-#define NUM_STR 1024
-#define STR_LEN 1000
+//#define NUM_STR 1024
+#define STR_LEN 20
+
+pthread_mutex_t array_lock;
+char** string_array;
 
 void *ServerEcho(void *args)
 {
@@ -19,12 +22,24 @@ void *ServerEcho(void *args)
 
     read(clientFileDescriptor,str,20);
     printf("reading from client:%s\n",str);
-    write(clientFileDescriptor,str,20);
-    close(clientFileDescriptor);
+    
     printf("does this work?\n");
     ClientRequest cr;
     ParseMsg(str, &cr);
-    printf("%s\n", cr.msg);
+    //printf("message parsed");
+    
+    if(!cr.is_read)
+    {
+        printf("getting mutex");
+        pthread_mutex_lock(&array_lock);
+        setContent(cr.msg, cr.pos, string_array);
+        char* temp;
+        getContent(temp, cr.pos, string_array);
+        printf(temp);
+        pthread_mutex_unlock(&array_lock);
+    }
+    write(clientFileDescriptor,cr.msg,20);
+    close(clientFileDescriptor);
 
     return NULL;
 }
@@ -35,27 +50,35 @@ int main (int argc, char* argv[])
         printf("Please pass in the correct number of arguments\n");
         return 0;
     }
+    pthread_mutex_init(&array_lock, NULL);
 
     int arr_size = atoi(argv[1]);
-    int server_ip = argv[2];
-    int server_port = argv[3];
+    char* server_ip = argv[2];
+    int server_port = atoi(argv[3]);
 
     struct sockaddr_in sock_var;
     int serverFileDescriptor=socket(AF_INET,SOCK_STREAM,0);
     int clientFileDescriptor;
     int i;
-    pthread_t t[20];
+    pthread_t t[1000];
 
-    sock_var.sin_addr.s_addr=inet_addr("127.0.0.1");
-    sock_var.sin_port=3000;
+    string_array = (char**) malloc(arr_size * sizeof(char*));
+    for (i = 0; i < arr_size; i ++){
+        string_array[i] = (char*) malloc(STR_LEN * sizeof(char));
+        sprintf(string_array[i], "String %d: initial value", i);
+        printf("%s\n\n", string_array[i]);
+    }
+
+    sock_var.sin_addr.s_addr=inet_addr(server_ip);
+    sock_var.sin_port=server_port;
     sock_var.sin_family=AF_INET;
     if(bind(serverFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
     {
         printf("socket has been created\n");
-        listen(serverFileDescriptor,2000); 
+        listen(serverFileDescriptor,2000);
         while(1)        //loop infinity
         {
-            for(i=0;i<20;i++)      //can support 20 clients at a time
+            for(i=0;i<1;i++)      //can support 1000 clients at a time
             {
                 clientFileDescriptor=accept(serverFileDescriptor,NULL,NULL);
                 printf("Connected to client %d\n",clientFileDescriptor);
