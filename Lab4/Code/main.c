6610e564632769d011179f867c64e9d1d4895771
Tyler Beckingham <tyler@Tylers-MacBook-Pro.local>
2021-03-19 17:02:14 -0600

    Serial implementation working

--- Lab4/Code/main.c
+++ Lab4/Code/main.c
@@ -6,10 +6,17 @@
 #include <math.h>
 #include "timer.h"
 #include "Lab4_IO.h"
+#include <mpi.h>
 
+#define DAMPING_FACTOR 0.85
+#define EPSILON 0.00001
 
 int nodecount;
+double damp_const;
 struct node *nodehead;
+double *last_results;
+double *results;
+double *contribution;
 
 int get_data()
 {
@@ -26,6 +33,45 @@ int get_data()
     return 0;
 }
 
+void init_values()
+{
+    int i;
+    last_results = malloc(nodecount * sizeof(double));
+    results = malloc(nodecount * sizeof(double));
+    contribution = malloc(nodecount * sizeof(double));
+
+    for(i = 0; i < nodecount; i++)
+    {
+        results[i] = 1.0 / nodecount;
+    }
+}
+
+void update_contributions()
+{
+    int i;
+    for ( i = 0; i < nodecount; ++i)
+    {
+        contribution[i] = results[i] / nodehead[i].num_out_links * DAMPING_FACTOR;
+    }
+}
+
+void solve()
+{
+    int i, j;
+    do{
+        vec_cp(results, last_results, nodecount);
+        // update the value
+        for ( i = 0; i < nodecount; ++i){
+            results[i] = 0;
+            for ( j = 0; j < nodehead[i].num_in_links; ++j)
+                results[i] += contribution[nodehead[i].inlinks[j]];
+            results[i] += damp_const;
+        }
+        // update and broadcast the contribution
+        update_contributions();
+    }while(rel_error(results, last_results, nodecount) >= EPSILON);
+}
+
 int main (int argc, char *argv[])
 {
     double start, end;
@@ -37,20 +83,32 @@ int main (int argc, char *argv[])
     }
 
     get_data();
-    printf("nodecount: %d\n", nodecount);
+    damp_const = (1.0 - DAMPING_FACTOR) / nodecount;
 
-    
+    init_values();
+
+    solve();
+
     for(i = 0; i < nodecount; i++)
     {
-        printf("Node %d\n", i);
-        printf("IncomingLinks: %d\n", nodehead[i].num_in_links);
-        for(j = 0; j < nodehead[i].num_in_links; j++)
-        {
-            printf("%d ", nodehead[i].inlinks[j]);
-        }
-        printf("\n");
-        printf("Num out links: %d\n", nodehead[i].num_out_links);
+        printf("Node %d: %f\n", i, results[i]);
     }
+
+    Lab4_saveoutput(results, nodecount, 0.0);
+
+    //printf("nodecount: %d\n", nodecount);
+
+    // for(i = 0; i < nodecount; i++)
+    // {
+    //     printf("Node %d\n", i);
+    //     printf("IncomingLinks: %d\n", nodehead[i].num_in_links);
+    //     for(j = 0; j < nodehead[i].num_in_links; j++)
+    //     {
+    //         printf("%d ", nodehead[i].inlinks[j]);
+    //     }
+    //     printf("\n");
+    //     printf("Num out links: %d\n", nodehead[i].num_out_links);
+    // }
     
     return 0;
 }
